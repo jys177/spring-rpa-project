a0b206344b16fc7062f86199c77bce034edad5f3
joyeongsang <josang4u@naver.com>
2021-04-24 14:26:45 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title,changeTitle] =useState(['남자 코트 추천','강남 우동 맛집','국힙 노래 추천']);
+
+  let navContentStyle={color:'white', fontSize:'30px'};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-nav">
+        <div style={navContentStyle}>개발 블로그 blog</div>
+      </div>
+      {/* <img src={logo}/> */}
+      <div className="list">
+        <h3>{title[0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        <h3>{title[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        <h3>{title[2]}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+        
+      </div>
+
     </div>
   );
 }
